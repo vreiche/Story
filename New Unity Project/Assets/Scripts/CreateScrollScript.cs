493c163370318c988b0f844325e4a082329a40e7
Alex <vreiche@yahoo.com>
2015-11-20 15:41:13 -0700

    This is Change, you better like it.
    yes yes it is.

---
+++ New Unity Project/Assets/Scripts/CreateScrollScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Item
+{
+    public string writeit;
+    public Button.ButtonClickedEvent thingToDo;
+    public Button.ButtonClickedEvent thingToDo2;
+}
+
+public class CreateScrollList : MonoBehaviour
+{
+
+    public GameObject newImage;
+    public List<Item> itemList;
+
+    public Transform contentPanel;
+
+    void Start()
+    {
+        PopulateList();
+    }
+
+    void PopulateList()
+    {
+        foreach (var item in itemList)
+        {
+            GameObject Image = Instantiate(newImage) as GameObject;
+        }
+    }
+
+    public void SomethingToDo()
+    {
+        Debug.Log("I done did something!");
+    }
+
+    public void SomethingElseToDo(GameObject item)
+    {
+        Debug.Log(item.name);
+    }
+}
